127a408c55a102656e78bcbafcc62a9149325919
ibraloyza <ibraahim3523@gmail.com>
2024-07-27 20:59:19 +0300

    added  page  html content bootstrap and external css

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>CRUD_APP</title>
+  </head>
+  <body>
+    <h1 id="main_title">CRUD APPLICATION IN PHP</h1>
+    <div class="container">
+        <h2 id="std_title">ALL STUDETNS</h2>
+      <table class="table table-hover table-bordered table-striped">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>first_name</th>
+            <th>last_name</th>
+            <th>age</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>ibrahim</td>
+            <td>hassan</td>
+            <td>24</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>said</td>
+            <td>A/rahman</td>
+            <td>23</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
