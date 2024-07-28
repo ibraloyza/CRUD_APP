2fac9ad991e12d9580963777c118c892629c4ac9
ibraloyza <ibraahim3523@gmail.com>
2024-07-28 21:51:02 +0300

    added deleted

---
+++ delete_page.php
@@ -0,0 +1,15 @@
+<?php include('dbcon.php');?>
+
+<?php
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $query = "delete from `students` where `id` = '$id'";
+    $result = mysqli_query($connection, $query);
+    if(!$result){
+        die("query failed" . mysqli_error());
+    }else{
+        header('location:index.php?delete_msg=you have deleted the data');
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -50,6 +50,17 @@
         echo "<h6>".$_GET['insert_data_msg']."</h6>";
     }
 ?>
+
+<?php
+    if(isset($_GET['update_msg'])){
+        echo "<h6>".$_GET['update_msg']."</h6>";
+    }
+?>
+<?php
+    if(isset($_GET['delete_msg'])){
+        echo "<h6>".$_GET['delete_msg']."</h6>";
+    }
+?>
 <!-- Modal -->
 <form action="insert_data.php" method="post">
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
